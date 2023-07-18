db4aefe169fadfe5435deaf0db0c9e29d1878a24
Steven Phung <sphung4035510@gmail.com>
2023-07-18 15:28:51 +1000

    for zhuman

--- keyboards/converter/adb_usb/rules.mk
+++ keyboards/converter/adb_usb/rules.mk
@@ -11,6 +11,7 @@ BACKLIGHT_ENABLE = no        # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no         # Enable keyboard RGB underglow
 AUDIO_ENABLE = no            # Audio output
 CUSTOM_MATRIX = yes
+UNICODEMAP_ENABLE = yes
 
 SRC += matrix.c adb.c led.c
 
