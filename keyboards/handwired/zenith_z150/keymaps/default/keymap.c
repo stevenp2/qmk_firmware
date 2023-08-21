188b8f47ee27e327e573b2c5dae70c2856def611
Steven Phung <sphung4035510@gmail.com>
2023-08-21 22:26:52 +1000

    led for extra toggle and move rgui

--- keyboards/handwired/zenith_z150/keymaps/default/keymap.c
+++ keyboards/handwired/zenith_z150/keymaps/default/keymap.c
@@ -21,14 +21,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `---------------------------------------------------------------------------------------------------------------------------'
 */
     [_BASE] = LAYOUT(
-    KC_F1,  KC_F2, KC_ESC,   KC_1,    KC_2,   KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   TG(_NUMLOCK),     KC_RGUI,       MO(_EXTRA),
+    KC_F1,  KC_F2, KC_ESC,   KC_1,    KC_2,   KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   TG(_NUMLOCK),     MO(_EXTRA),    KC_RGUI,
     KC_F3,  KC_F4, KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,            KC_HOME,   KC_UP,     KC_PGUP,   KC_PAST,
     KC_F5,  KC_F6, KC_LCTL,  KC_A,    KC_S,   KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,    KC_LEFT,   KC_P5,     KC_RGHT,   KC_PMNS,
     KC_F7,  KC_F8, KC_LSFT,           KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_BSLS,   KC_END,    KC_DOWN,   KC_PGDN,   KC_PPLS,
     KC_F9,  KC_F10,KC_LALT,         KC_GRV,                                 KC_SPC,                                            KC_CAPS,       KC_INS,           KC_DEL
     ),
     [_NUMLOCK] = LAYOUT(
-    KC_F1,  KC_F2, KC_ESC,   KC_1,    KC_2,   KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   TG(_NUMLOCK),     KC_RGUI,       MO(_EXTRA),
+    KC_F1,  KC_F2, KC_ESC,   KC_1,    KC_2,   KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   TG(_NUMLOCK),     MO(_EXTRA),    KC_RGUI,
     KC_F3,  KC_F4, KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,            KC_P7,   KC_P8,       KC_P9,     KC_PAST,
     KC_F5,  KC_F6, KC_LCTL,  KC_A,    KC_S,   KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,    KC_P4,   KC_P5,       KC_P6,     KC_PMNS,
     KC_F7,  KC_F8, KC_LSFT,           KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_BSLS,   KC_P1,   KC_P2,       KC_P3,     KC_PPLS,
@@ -51,19 +51,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     K25,    K24,   K40,            K55,                                    K37,											   K36,           K06,              K05 \
 */
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-    case KC_RGUI:
-        if (record->event.pressed) {
-            writePinLow(LED_SCROLL_LOCK_PIN);
-        } else {
-            writePinHigh(LED_SCROLL_LOCK_PIN);
-        }
-        return true; // Skip all further processing of this key
-    }
-    return true;
-}
-
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//     switch (keycode) {
+//     case KC_RGUI:
+//         if (record->event.pressed) {
+//             writePinLow(LED_SCROLL_LOCK_PIN);
+//         } else {
+//             writePinHigh(LED_SCROLL_LOCK_PIN);
+//         }
+//         return true;
+//     }
+//     return true;
+// }
 
 layer_state_t layer_state_set_kb(layer_state_t state) {
     if (IS_LAYER_ON_STATE(state, _NUMLOCK)) {
@@ -72,5 +71,13 @@ layer_state_t layer_state_set_kb(layer_state_t state) {
     else {
         writePinHigh(LED_NUM_LOCK_PIN);
     }
+
+    if (IS_LAYER_ON_STATE(state, _EXTRA)) {
+        writePinLow(LED_SCROLL_LOCK_PIN);
+    }
+    else {
+        writePinHigh(LED_SCROLL_LOCK_PIN);
+    }
+
     return state;
 }
