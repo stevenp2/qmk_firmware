33a56d149a646ff9dafab3847709ea68d080d44e
Steven Phung <sphung4035510@gmail.com>
2023-08-21 20:38:10 +1000

    z150 firmware

---
+++ keyboards/handwired/zenith_z150/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define LED_NUM_LOCK_PIN C7
+#define LED_SCROLL_LOCK_PIN D4
+#define LED_CAPS_LOCK_PIN D5
+
---
+++ keyboards/handwired/zenith_z150/z150.c
@@ -0,0 +1,28 @@
+#include "quantum.h"
+
+void led_init_ports(void) {
+    setPinOutput(LED_NUM_LOCK_PIN);
+    setPinOutput(LED_CAPS_LOCK_PIN);
+    setPinOutput(LED_SCROLL_LOCK_PIN);
+
+    writePinLow(LED_CAPS_LOCK_PIN);
+    writePinLow(LED_SCROLL_LOCK_PIN);
+}
+
+void matrix_init_kb(void) {
+    matrix_init_user();
+}
+
+void led_update_ports(led_t led_state) {
+    if (led_state.caps_lock) {
+        writePinLow(LED_CAPS_LOCK_PIN);
+    } else {
+        writePinHigh(LED_CAPS_LOCK_PIN);
+    }
+
+    if (led_state.scroll_lock) {
+        writePinLow(LED_SCROLL_LOCK_PIN);
+    } else {
+        writePinHigh(LED_SCROLL_LOCK_PIN);
+    }
+}
