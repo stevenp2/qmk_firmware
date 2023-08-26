faa2c931461f173f88cef8fab9f4c9131345741b
Steven Phung <sphung4035510@gmail.com>
2023-08-26 17:58:37 +1000

    annoy coworkers

--- keyboards/handwired/zenith_z150/keymaps/default/keymap.c
+++ keyboards/handwired/zenith_z150/keymaps/default/keymap.c
@@ -6,6 +6,8 @@ enum layer_names {
     _EXTRA,
 };
 
+extern void beep(uint32_t frequency, uint32_t duration);
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * ,---------------------------------------------------------------------------------------------------------------------------.
@@ -25,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F3,  KC_F4, KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,            KC_HOME,   KC_UP,     KC_PGUP,   KC_PAST,
     KC_F5,  KC_F6, KC_LCTL,  KC_A,    KC_S,   KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,    KC_LEFT,   KC_P5,     KC_RGHT,   KC_PMNS,
     KC_F7,  KC_F8, KC_LSFT,           KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_BSLS,   KC_END,    KC_DOWN,   KC_PGDN,   KC_PPLS,
-    KC_F9,  KC_F10,KC_LALT,         KC_GRV,                                 KC_SPC,                                            KC_CAPS,       KC_INS,           KC_DEL
+    KC_F9,  KC_F10,LT(_EXTRA, KC_LALT),         KC_GRV,                                 KC_SPC,                                            KC_CAPS,       KC_INS,           KC_DEL
     ),
     [_NUMLOCK] = LAYOUT(
     KC_F1,  KC_F2, KC_ESC,   KC_1,    KC_2,   KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   TG(_NUMLOCK),     MO(_EXTRA),    KC_RGUI,
@@ -35,11 +37,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F9,  KC_F10,KC_LALT,         KC_GRV,                                 KC_SPC,                                            KC_CAPS,       KC_P0,            KC_PDOT
     ),
     [_EXTRA] = LAYOUT(
-    KC_NO,  KC_NO, KC_NO,   KC_1,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,     KC_NO,            KC_NO,         KC_NO,
-    KC_NO,  KC_NO, KC_NO,   KC_Q,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,              KC_NO,   KC_NO,       KC_NO,     KC_MUTE,
-    KC_NO,  KC_NO, KC_NO,   KC_A,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_NO,            KC_NO,     KC_NO,   KC_NO,       KC_NO,     KC_VOLD,
-    KC_NO,  KC_NO, KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,       KC_NO,     KC_VOLU,
-    KC_NO,  KC_NO, KC_NO,            KC_NO,                                 QK_BOOT,                                           KC_NO,         KC_BRID,          KC_BRIU
+    KC_NO,  KC_NO, AU_TOGG, KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,   QK_BOOT,   KC_NO,            KC_NO,         KC_NO,
+    KC_NO,  KC_NO, KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,              KC_NO,   KC_NO,       KC_NO,     KC_MUTE,
+    KC_NO,  KC_NO, KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_NO,            KC_NO,     KC_NO,   KC_NO,       KC_NO,     KC_VOLD,
+    KC_NO,  KC_NO, KC_NO,             KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,       KC_NO,     KC_VOLU,
+    KC_NO,  KC_NO, KC_NO,            KC_NO,                                 KC_NO,                                             KC_NO,         KC_BRID,          KC_BRIU
     )
 };
 
@@ -64,6 +66,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //     return true;
 // }
 
+#ifdef AUDIO_ENABLE
+float sound[][2] = SONG(TERMINAL_SOUND);
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    PLAY_SONG(sound);
+  }
+  return true;
+}
+#endif
+
 layer_state_t layer_state_set_kb(layer_state_t state) {
     if (IS_LAYER_ON_STATE(state, _NUMLOCK)) {
         writePinLow(LED_NUM_LOCK_PIN);
@@ -81,3 +93,5 @@ layer_state_t layer_state_set_kb(layer_state_t state) {
 
     return state;
 }
+
+// QK_AUDIO_TOGGLE
